7b8df505e058f5297f1453b14d0778a9e9e335fc
ASUS <waghmodeajinkya80@gmail.com>
2023-03-29 23:31:09 +0530

    splash and UI for login and registration

---
+++ app/src/main/java/com/example/makechat/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.makechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+   FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firebaseAuth=FirebaseAuth.getInstance();
+        if(firebaseAuth==null){
+            startActivity(new Intent(MainActivity.this,login.class));
+        }
+    }
+}
